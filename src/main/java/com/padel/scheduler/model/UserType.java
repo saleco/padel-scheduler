2d93d3711af4c1d9ac388058c76b7cbb029d4a58
João Pina <joao_pinaa@hotmail.com>
2021-02-23 19:18:14 +0000

    initial commit

--- src/main/java/com/padel/scheduler/model/UserType.java
+++ src/main/java/com/padel/scheduler/model/UserType.java
@@ -1,9 +1,18 @@
 package com.padel.scheduler.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserType {
 
     @Id
